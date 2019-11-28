71c93916cdfbbb650b531ecceaa2cd14d473548a
Adria-F <adriaferrer2898@gmail.com>
2019-11-28 18:50:57 +0100

    increased packet size

--- Multiplayer Game/Source/Networks.h
+++ Multiplayer Game/Source/Networks.h
@@ -40,7 +40,7 @@
 #define SCENE_TRANSITION_TIME_SECONDS                   1.0f
 #define DISCONNECT_TIMEOUT_SECONDS                      5.0f
 #define PACKET_DELIVERY_TIMEOUT_SECONDS                 0.5f
-#define PACKET_SIZE                             Kilobytes(4)
+#define PACKET_SIZE                             Kilobytes(8)
 #define PING_INTERVAL_SECONDS                           0.5f
 #define REPLICATION_INTERVAL_SECONDS					0.128f
 
